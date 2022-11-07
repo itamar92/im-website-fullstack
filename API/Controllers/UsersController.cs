08c989228460353a4af2cf92a7171c37bfe1e10e
Itamar <itamar92@gmail.com>
2022-11-07 19:21:53 +0200

    Adding CORS to API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,18 +21,17 @@ namespace API.Controllers
         }
 
         [HttpGet] // api/users
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
             return users;
         }
 
         [HttpGet("{id}")] // api/users/1
 
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = _context.Users.Find(id);
-            return user; 
+            return await _context.Users.FindAsync(id);
         }
     }
 }
