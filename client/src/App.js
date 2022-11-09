cd89f74a11ba23692409ec8b17764a1b45a886fc
Itamar <itamar92@gmail.com>
2022-11-09 10:43:46 +0200

    Connecting Api to Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import {useEffect} from 'react';
-import axios, {AxiosResponse} from 'axios';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import "./App.css";
 
 function App() {
-
-  useEffect(() => {
-    axios.get('http://localhost:5001//api/users')
-    .then(( AxiosResponse  => {
+  useEffect(function () {
+    axios.get("https://localhost:5001/api/users").then(function (response) {
       console.log(response.data);
-    }))
-  },[]) 
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
