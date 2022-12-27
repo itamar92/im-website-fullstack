b3b8ba1eee041a71192ddd77565ccbd57df0a7bb
Itamar <itamar92@gmail.com>
2022-12-27 14:57:51 +0200

    Adding ITokenService

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios, { AxiosResponse } from "axios";
 import { urlUsers } from "./endpoints";
 
 function App() {
+  const [users, setUsers] = useState([]);
 
-  const users = () => {
-    axios
-    .get(urlUsers)
-    .then((response: AxiosResponse<any>) => {
-      console.log(response.data);
-    });
-  }
-
-  
   useEffect(() => {
-   users();
+    axios.get(urlUsers).then((response: AxiosResponse<any>) => {
+      setUsers(response.data);
+      console.log(users);
+    
+    });
   }, []);
+
+  console.log(users);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      
+      <p>Hello</p>
     </div>
   );
 }
