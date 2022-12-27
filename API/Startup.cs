65c2978adb91ab37394b1a186cc25da6f2bef4ed
Itamar <itamar92@gmail.com>
2022-12-27 15:16:35 +0200

    Adding Extension methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -32,12 +33,8 @@ namespace API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITokenService, TokenService>();
+            services.AddApplicationServices(_config); 
 
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -46,16 +43,9 @@ namespace API
             });
 
             services.AddCors();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options => {//  configure parameters 
-                options.TokenValidationParameters = new TokenValidationParameters{
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                    ValidateIssuer = false, // the api server 
-                    ValidateAudience = false // the REACT app
-                    
-                };
-            });
+
+             services.AddIdentityServices(_config);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
