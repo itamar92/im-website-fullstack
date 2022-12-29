5fc4e1b2fe2b568922c983d921be9916a684c298
Itamar <itamar92@gmail.com>
2022-12-29 12:09:28 +0200

    adding token key

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,15 @@ import axios, { AxiosResponse } from "axios";
 import { urlUsers } from "./endpoints";
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([{
+    id:"",
+    userName:""
+  }]);
 
   useEffect(() => {
     axios.get(urlUsers).then((response: AxiosResponse<any>) => {
       setUsers(response.data);
-      console.log(users);
-    
+      //console.log(users);
     });
   }, []);
 
@@ -20,6 +22,12 @@ function App() {
   return (
     <div className="App">
       <p>Hello</p>
+      {users &&
+        users.map((user) => (
+          <li key={user.id}>
+            {user.id} - {user.userName}
+          </li>
+        ))}
     </div>
   );
 }
